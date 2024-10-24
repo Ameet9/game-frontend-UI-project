872fd81b5428d180a0f23a7b22356366014125c7
Ameet9 <ameetnayak919@gmail.com>
2024-10-24 21:02:05 +0530

    UI updated

--- src/pages/GameRoom.jsx
+++ src/pages/GameRoom.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { io } from "socket.io-client";
+import React, { useEffect } from "react";
 
 const socket = io.connect("http://localhost:5000");
 
@@ -8,16 +8,43 @@ function GameRoom() {
   const { roomId } = useParams();
 
   useEffect(() => {
-    socket.emit("joinRoom", roomId);
+    socket.on("connect", () => {
+      console.log("Connected to the server");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from the server");
+    });
+
     socket.on("message", (message) => {
       console.log(message);
     });
-  }, [roomId]);
 
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("message");
+    };
+  }, []);
+  // const userJoinRoom = () => {
+  //   console.log("Attempting to join room:", roomId);
+  //   socket.emit("joinRoom", roomId);
+  //   socket.on("message", (message) => {
+  //     console.log(message);
+  //   });
+  // };
+  // const findAllRooms = () => {
+  //   socket.emit("findAllRooms", roomId);
+  //   socket.on("message", (message) => {
+  //     console.log(message);
+  //   });
+  // };
   return (
     <div>
       <h1>Room Id: {roomId}</h1>
-      <p>Game willl start...</p>
+      <p>Game will start...</p>
+      {/* <button onClick={userJoinRoom}>Join</button> */}
+      {/* <button onClick={findAllRooms}>all rooms</button> */}
     </div>
   );
 }
--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,25 +1,72 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+
+const socket = io.connect("http://localhost:5000");
 
 function Lobby() {
   const [roomCode, setRoomCode] = useState("");
+  const [userName, setUserName] = useState("");
+  const [securityCode, setSecurityCode] = useState("");
+  const [isNewRoom, setIsNewRoom] = useState(false);
   const nav = useNavigate();
 
   const handleJoinRoom = () => {
-    if (roomCode) {
-      nav(`/room/${roomCode}`);
+    if (roomCode && userName) {
+      socket.emit(
+        "joinRoom",
+        {
+          roomId: roomCode,
+          userName: userName,
+          securityCode: isNewRoom ? "" : securityCode,
+        },
+        (response) => {
+          if (response.status === "success") {
+            alert(response.message);
+            if (isNewRoom) {
+              alert("Security Code is:- " + response.securityCode);
+            }
+            nav(`/room/${roomCode}`);
+          } else {
+            alert(response.status);
+          }
+        }
+      );
     }
   };
   return (
     <>
       <center>
+        <h2>{isNewRoom ? "Create a Room" : "Join a Room"} To Play</h2>
         <input
           type="text"
           placeholder="Enter Room Code"
           value={roomCode}
           onChange={(e) => setRoomCode(e.target.value)}
         />
-        <button onClick={handleJoinRoom}>Join Room</button>
+        <input
+          type="text"
+          placeholder="Enter User Name"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        {!isNewRoom && (
+          <input
+            type="text"
+            placeholder="Enter Security Code"
+            value={securityCode}
+            onChange={(e) => setSecurityCode(e.target.value)}
+          ></input>
+        )}
+        <button onClick={handleJoinRoom}>
+          {isNewRoom ? "Create Room" : "Join Room"}
+        </button>
+        <p>
+          {isNewRoom ? "Already have a room? " : "New here? "}
+          <button onClick={() => setIsNewRoom(!isNewRoom)}>
+            {isNewRoom ? "Join existing room" : "Create new room"}
+          </button>
+        </p>
       </center>
     </>
   );
